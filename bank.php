f1e58784856e8f2b1fb5623e0f2079ac9a128bfa
Adrian Grunwell <grunners22@hotmail.com>
2020-05-28 10:35:40 +0100

    ajax added and working

--- bank.php
+++ bank.php
@@ -1,64 +1,48 @@
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <?php
 session_start();
-require __DIR__ . '/vendor/autoload.php';
 
-//bank modulus
-use Cs278\BankModulus\BankModulus;
-use Cs278\BankModulus\Exception\AccountNumberInvalidException;
-use Cs278\BankModulus\Exception\SortCodeInvalidException;
-
-//monolog
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-$logger = new Logger('Bank-Modulus');
-$logger->pushHandler(new StreamHandler(__DIR__ .'/bank.log', Logger::INFO));
-$logger->pushProcessor (function ($entry) {
-    $entry['extra']['session_id'] = session_id();
-    return $entry;
-});
 
 ?>
 
 <form method="post">
 	<h3>Account Details</h3>
-    <input type="text" name="txtSortCode" placeholder="Sort code" <?php if(isset($_POST['txtSortCode'])) { echo 'value="' . $_POST['txtSortCode'] . '"'; } else { echo 'value="774814"'; } ?>><br>
-    <input type="text" name="txtAccNumber" placeholder="Account number" <?php if(isset($_POST['txtAccNumber'])) { echo 'value="' . $_POST['txtAccNumber'] . '"'; } else { echo 'value="24782346"'; } ?>><br>
-	<input name="submitAccountCheck" type="submit" value="Check" />
+    <input type="text" name="txtSortCode" id="txtSortCode" placeholder="Sort code" <?php if(isset($_POST['txtSortCode'])) { echo 'value="' . $_POST['txtSortCode'] . '"'; } else { echo 'value="774814"'; } ?>><br>
+    <input type="text" name="txtAccNumber" id="txtAccNumber" placeholder="Account number" <?php if(isset($_POST['txtAccNumber'])) { echo 'value="' . $_POST['txtAccNumber'] . '"'; } else { echo 'value="24782346"'; } ?>><br>
+    <div id="message"></div>
+</form>	
 
-	<?php 
-		if (array_key_exists('submitAccountCheck',$_POST)) {
-            $sortcode = $_POST['txtSortCode'];
-            $accountnumber = $_POST['txtAccNumber'];
-            $modulus = new BankModulus();
-            //var_dump($accountnumber);
-            try {
-                $modulus->normalize($sortcode, $accountnumber);
-                $modulus = $modulus->check($sortcode, $accountnumber);
-                if($modulus) {
-                    echo "Valid" . "<br><br>";
-                } else {
-                    echo "INVALID" . "<br><br>";
-                    $logger->info('Invalid Bank Details: ' . $sortcode . " " . $accountnumber);
-                };
-            } catch (SortCodeInvalidException $e) {
-                echo "Sort code exception";
-                $logger->info('Sort code exception: ' . $sortcode);
-            } catch (AccountNumberInvalidException $e) {
-                echo "Account number exception";
-                $logger->info('Account number exception: ' . $accountnumber);
-            } catch (Exception $e ) {
-                echo "Other exception<p>" . $e->getMessage();
-                $logger->info('General exception: ' . $e->getMessage());
+<script type="text/javascript">
+
+$(function() {
+    
+    var bankInvalid = []; //declared outside so available globally
+    function validateBankNumber(sortcode, accnum) {
+        $.ajax({
+            url: '/func-ajax-modulus.php',
+            type: 'POST',
+            data: {
+                SortCode: sortcode,
+                AccNum: accnum
+            },
+            success: function(data) {
+                var result = data.result;
+                console.log(data);
+                $('#message').html(result);
+            },
+            error: function(data) {
+                console.log(data);
             }
-            
-            //var_dump($sortcode, $accountnumber);
+        });
+    }
+    
+    //onblur modulus check
+    $('#txtAccNumber, #txtSortCode').bind('blur', function() {
+        if ( $('#txtAccNumber').val() && $('#txtSortCode').val() ) {
+            validateBankNumber($('#txtSortCode').val(), $('#txtAccNumber').val());
+        }
+    });
+});
 
-            
-            
-            
-
-		}
-	?>
-
-</form>	</form>
+</script>
 
---
+++ func-ajax-modulus.php
@@ -0,0 +1,77 @@
+<?php
+
+//ajax required
+if(!isset($_SESSION)) { session_start(); } 
+header('Content-Type: application/json');
+date_default_timezone_set('Europe/London');
+/*************************************** */
+require __DIR__ . '/vendor/autoload.php';
+
+//bank modulus
+use Cs278\BankModulus\BankModulus;
+use Cs278\BankModulus\Exception\AccountNumberInvalidException;
+use Cs278\BankModulus\Exception\SortCodeInvalidException;
+
+//monolog
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+function modulusCheck() {
+    $logger = new Logger('Bank-Modulus');
+    $logger->pushHandler(new StreamHandler(__DIR__ .'/bank.log', Logger::INFO));
+    $logger->pushProcessor (function ($entry) {
+        $entry['extra']['session_id'] = session_id();
+        return $entry;
+    });
+    $sortcode = $_POST['SortCode'];
+    $accountnumber = $_POST['AccNum'];
+
+    $modulus = new BankModulus();
+    //var_dump($accountnumber);
+    try {
+        $modulus->normalize($sortcode, $accountnumber);
+        $modulus = $modulus->check($sortcode, $accountnumber);
+        if($modulus) {
+            $message = "Valid" . "<br><br>";
+        } else {
+            $message = "INVALID" . "<br><br>";
+            $logger->info('Invalid Bank Details: ' . $sortcode . " " . $accountnumber);
+        };
+    } catch (SortCodeInvalidException $e) {
+        $message = "Sort code exception";
+        $logger->info('Sort code exception: ' . $sortcode);
+    } catch (AccountNumberInvalidException $e) {
+        $message = "Account number exception";
+        $logger->info('Account number exception: ' . $accountnumber);
+    } catch (Exception $e ) {
+        $message = "Other exception<p>" . $e->getMessage();
+        $logger->info('General exception: ' . $e->getMessage());
+    }
+
+    return $message;
+}
+
+$thisvar = modulusCheck();
+
+$response_array = array(
+    "result" => $thisvar
+);
+
+//return the result
+if(isset($response_array)) {
+    echo json_encode($response_array);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
